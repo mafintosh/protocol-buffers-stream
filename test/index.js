60619fdda1a1c8cb9c83e116e202ef8d3483805d
Mathias Buus <mathiasbuus@gmail.com>
2014-08-09 08:45:23 +0200

    no handshake test

--- test/index.js
+++ test/index.js
@@ -102,6 +102,53 @@ tape('multi type messages', function(t) {
   stream.pipe(stream)
 })
 
+tape('multi type messages + no handshake', function(t) {
+  t.plan(6)
+
+  var stream = createStream({handshake:false})
+
+  var tests = [{
+    foo: new Buffer('one')
+  }, {
+    foo: new Buffer('two')
+  }]
+
+  var anothers = [{
+    test: 'hej'
+  }, {
+    test: ''
+  }]
+
+  var anotherOnes = [{
+    foo: 1,
+    bar: 'foo'
+  }, {
+    foo: 100,
+    bar: 'bar'
+  }]
+
+
+  stream.anotherOne(anotherOnes[0])
+  stream.anotherOne(anotherOnes[1])
+  stream.test(tests[0])
+  stream.another(anothers[0])
+  stream.test(tests[1])
+  stream.another(anothers[1])
+
+  stream.on('anotherOne', function(m) {
+    t.same(m, anotherOnes.shift())
+  })
+
+  stream.on('test', function(m) {
+    t.same(m, tests.shift())
+  })
+
+  stream.on('another', function(m) {
+    t.same(m, anothers.shift())
+  })
+
+  stream.pipe(stream)
+})
 
 tape('chunked 1 message', function(t) {
   var stream = createStream()
